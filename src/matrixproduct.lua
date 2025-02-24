6436fce4ae5402da4ac4206b34989e94d4134cf2
Process-ing <rbruno.p.o@gmail.com>
2025-02-24 08:50:47 +0000

    Remove block multiplication from Lua script

--- src/matrixproduct.lua
+++ src/matrixproduct.lua
@@ -70,37 +70,12 @@ local function on_mult_line(m, n, p)
     print_first_elems(mat_c, n, p);
 end
 
-local function on_mult_block(m, n, p, blockSize)
-    local mat_a = init_array(m, n, true)
-    local mat_b = init_array(n, p, true)
-    local mat_c = init_array(m, p, false)
-
-    for row = 0, math.ceil(m / blockSize) - 1 do
-        for col = 0, math.ceil(p / blockSize) - 1 do
-            for i = row * blockSize, math.min((row + 1) * blockSize, m) - 1 do
-                for k = 0, n - 1 do
-                    for j = col * blockSize, math.min((col + 1) * blockSize, p) - 1 do
-                        if mat_b[k * p + j + 1] == nil then
-                            print(i, j, k, n * p, k * p + j + 1)
-                        end
-                        mat_c[i * p + j + 1] = mat_c[i * p + j + 1] + mat_a[i * n + k + 1] * mat_b[k * p + j + 1];
-                    end
-                end
-            end
-        end
-    end
-
-    print("Result matrix:")
-    print_first_elems(mat_c, n, p);
-end
-
 local function print_usage()
-    print("Usage: lua matrixproduct.lua <op> <lin> <col> <output> [blockSize]")
+    print("Usage: lua matrixproduct.lua <op> <lin> <col> <output>")
     print("  <op>       : Opration mode: 1, 2, 3 (required)")
     print("  <lin>      : Number of lines (required)")
     print("  <col>      : Number of columns (required)")
     print("  <output>   : Path to output filename (required)")
-    print("  [blockSize]: Size of a block (optional)")
 end
 
 local function open_file(filename)
@@ -123,24 +98,18 @@ local function open_file(filename)
 end
 
 local function main()
-    if #arg < 4 or #arg > 5 then
+    if #arg ~= 4 then
         print_usage()
         return
     end
 
     local op = tonumber(arg[1]);
-    if op ~= 3 and #arg > 4 then
-        print_usage()
-        return
-    end
-
     local line = tonumber(arg[2])
     local col = tonumber(arg[3])
     local filename = arg[4]
     local file = open_file(filename)
-    local block_size = tonumber(arg[5]);
 
-    if line == nil or col == nil or (op == 3 and block_size == nil) then
+    if line == nil or col == nil then
         print_usage()
         return
     end
@@ -150,15 +119,13 @@ local function main()
         time = time_func(function () on_mult(line, col, line) end)
     elseif op == 2 then
         time = time_func(function () on_mult_line(line, col, line) end)
-    elseif op == 3 then
-        time = time_func(function () on_mult_block(line, col, line, block_size) end)
     else
         print_usage()
         return
     end
 
     print("Time:", time, "ms")
-    file:write(table.concat({op, line, col, block_size, time}, ",") .. "\n")
+    file:write(table.concat({op, line, col, 0.0, time}, ",") .. "\n")
 end
 
 main()
