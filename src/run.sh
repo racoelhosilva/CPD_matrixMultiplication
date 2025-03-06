579e0db8ee65a5cf1dc422bdaec22378e0b329a5
Rodrigo Silva <up202205188@up.pt>
2025-03-06 15:01:20 +0000

    feat: omp parallel implementations

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-g++ -O2 matrixproduct.cpp -o matrixproduct -lpapi
+g++ -O2 matrixproduct.cpp -o matrixproduct -lpapi -fopenmp
 
 for size in $(seq 600 400 3000); do
     ./matrixproduct "output/test-cpp-1-${size}.csv" 1 "$size"
@@ -14,6 +14,14 @@ for size in $(seq 4096 2048 10240); do
     for block_size in "128" "256" "512"; do
         ./matrixproduct "output/test-cpp-3-${size}-${block_size}.csv" 3 "$size" "$block_size"
     done
+done
+
+for size in $(seq 600 400 3000) $(seq 4096 2048 10240); do
+    ./matrixproduct "output/test-cpp-4-${size}.csv" 4 "$size"
+done
+
+for size in $(seq 600 400 3000) $(seq 4096 2048 10240); do
+    ./matrixproduct "output/test-cpp-5-${size}.csv" 5 "$size"
 done
 
 for size in $(seq 600 400 3000); do
