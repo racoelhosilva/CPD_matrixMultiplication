da478f83956c6c1852c5e4261128be219d518272
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-03-06 10:58:08 +0000

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -3,16 +3,16 @@
 g++ -O2 matrixproduct.cpp -o matrixproduct -lpapi
 
 for size in $(seq 600 400 3000); do
-    ./matrixproduct 1 "$size" "output/test-cpp-1-${size}.csv"
+    ./matrixproduct "output/test-cpp-1-${size}.csv" 1 "$size"
 done
 
 for size in $(seq 600 400 3000) $(seq 4096 2048 10240); do
-    ./matrixproduct 2 "$size" "output/test-cpp-2-${size}.csv"
+    ./matrixproduct "output/test-cpp-2-${size}.csv" 2 "$size"
 done
 
 for size in $(seq 4096 2048 10240); do
     for block_size in "128" "256" "512"; do
-        ./matrixproduct 3 "$size" "output/test-cpp-3-${size}-${block_size}.csv" "$block_size"
+        ./matrixproduct "output/test-cpp-3-${size}-${block_size}.csv" 3 "$size" "$block_size"
     done
 done
 
