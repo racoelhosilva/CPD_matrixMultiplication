248118159ba397b2ca78d23fc8b31db5eb967bdf
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-03-15 22:11:51 +0000

    Generate more charts

--- doc/gen-charts.py
+++ doc/gen-charts.py
@@ -33,7 +33,7 @@ def import_csv_files(folder_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     return (cpp_df, lua_df)
 
-def plot_lang_comp(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
+def plot_naive_line_time(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
     naive_cpp_df = cpp_df[cpp_df['OPERATION_MODE'] == 1].groupby('SIZE').median().reset_index()
     naive_lua_df = lua_df[lua_df['OPERATION_MODE'] == 1].groupby('SIZE').median().reset_index()
     line_cpp_df = cpp_df[(cpp_df['OPERATION_MODE'] == 2) & (cpp_df['SIZE'] <= 3000)].groupby('SIZE').median().reset_index()
@@ -48,13 +48,186 @@ def plot_lang_comp(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
         plt.plot(line_lua_df['SIZE'], line_lua_df['TIME'], label='Line Lua', marker='.')
 
         plt.xticks(range(600, 3001, 400))
+        plt.ylim(bottom=0)
 
-        plt.title('Naive vs Line by Language')
+        plt.title('Execution Time of Naive vs Line by Language')
         plt.xlabel('Size')
         plt.ylabel('Time (s)')
         plt.legend()
 
-        output_path = os.path.join(output_folder, 'naive-line.pdf')
+        output_path = os.path.join(output_folder, 'naive-line-time.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_naive_line_flops(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
+    naive_cpp_df = cpp_df[cpp_df['OPERATION_MODE'] == 1].groupby('SIZE').median().reset_index()
+    naive_lua_df = lua_df[lua_df['OPERATION_MODE'] == 1].groupby('SIZE').median().reset_index()
+    line_cpp_df = cpp_df[(cpp_df['OPERATION_MODE'] == 2) & (cpp_df['SIZE'] <= 3000)].groupby('SIZE').median().reset_index()
+    line_lua_df = lua_df[lua_df['OPERATION_MODE'] == 2].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(naive_cpp_df['SIZE'], naive_cpp_df['MFLOPS'], label='Naive C++', marker='.', )
+        plt.plot(naive_lua_df['SIZE'], naive_lua_df['MFLOPS'], label='Naive Lua', marker='.')
+        plt.plot(line_cpp_df['SIZE'], line_cpp_df['MFLOPS'], label='Line C++', marker='.')
+        plt.plot(line_lua_df['SIZE'], line_lua_df['MFLOPS'], label='Line Lua', marker='.')
+
+        plt.xticks(range(600, 3001, 400))
+        plt.ylim(bottom=0)
+
+        plt.title('Mflops of Naive vs Line by Language')
+        plt.xlabel('Size')
+        plt.ylabel('Mflops')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'naive-line-flops.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_line_block_time(df: pd.DataFrame) -> None:
+    line_df = df[(df['OPERATION_MODE'] == 2) & (df['SIZE'] >= 4096)].groupby('SIZE').median().reset_index()
+    block_128_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 128)].groupby('SIZE').median().reset_index()
+    block_256_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 256)].groupby('SIZE').median().reset_index()
+    block_512_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 512)].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(line_df['SIZE'], line_df['TIME'], label='Line', marker='.')
+        plt.plot(block_128_df['SIZE'], block_128_df['TIME'], label='Block-128', marker='.')
+        plt.plot(block_256_df['SIZE'], block_256_df['TIME'], label='Block-256', marker='.')
+        plt.plot(block_512_df['SIZE'], block_512_df['TIME'], label='Block-512', marker='.')
+
+        plt.xticks(range(4096, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Execution Time of Line and Block per Block Size')
+        plt.xlabel('Size')
+        plt.ylabel('Time (s)')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'line-block-time.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_line_block_flops(df: pd.DataFrame) -> None:
+    line_df = df[(df['OPERATION_MODE'] == 2) & (df['SIZE'] >= 4096)].groupby('SIZE').median().reset_index()
+    block_128_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 128)].groupby('SIZE').median().reset_index()
+    block_256_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 256)].groupby('SIZE').median().reset_index()
+    block_512_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 512)].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(line_df['SIZE'], line_df['MFLOPS'], label='Line', marker='.')
+        plt.plot(block_128_df['SIZE'], block_128_df['MFLOPS'], label='Block-128', marker='.')
+        plt.plot(block_256_df['SIZE'], block_256_df['MFLOPS'], label='Block-256', marker='.')
+        plt.plot(block_512_df['SIZE'], block_512_df['MFLOPS'], label='Block-512', marker='.')
+
+        plt.xticks(range(4096, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Mflops of Line and Block per Block Size')
+        plt.xlabel('Size')
+        plt.ylabel('Mflops')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'line-block-flops.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_parallel_time(df: pd.DataFrame) -> None:
+    line_df = df[df['OPERATION_MODE'] == 2].groupby('SIZE').median().reset_index()
+    parallel_1_df = df[df['OPERATION_MODE'] == 4].groupby('SIZE').median().reset_index()
+    parallel_2_df = df[df['OPERATION_MODE'] == 5].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(line_df['SIZE'], line_df['TIME'], label='Line', marker='.')
+        plt.plot(parallel_1_df['SIZE'], parallel_1_df['TIME'], label='Parallel V.1', marker='.')
+        plt.plot(parallel_2_df['SIZE'], parallel_2_df['TIME'], label='Parallel V.2', marker='.')
+
+        plt.xticks(range(0, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Execution Time of Sequential vs Parallel Line')
+        plt.xlabel('Size')
+        plt.ylabel('Time (s)')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'parallel-time.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_parallel_flops(df: pd.DataFrame) -> None:
+    line_df = df[df['OPERATION_MODE'] == 2].groupby('SIZE').median().reset_index()
+    parallel_1_df = df[df['OPERATION_MODE'] == 4].groupby('SIZE').median().reset_index()
+    parallel_2_df = df[df['OPERATION_MODE'] == 5].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(line_df['SIZE'], line_df['MFLOPS'], label='Line', marker='.')
+        plt.plot(parallel_1_df['SIZE'], parallel_1_df['MFLOPS'], label='Parallel V.1', marker='.')
+        plt.plot(parallel_2_df['SIZE'], parallel_2_df['MFLOPS'], label='Parallel V.2', marker='.')
+
+        plt.xticks(range(0, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Mflops of Sequential vs Parallel Line')
+        plt.xlabel('Size')
+        plt.ylabel('Mflops')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'parallel-flops.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_parallel_speedup(df: pd.DataFrame) -> None:
+    line_df = df[df['OPERATION_MODE'] == 2].groupby('SIZE').median().reset_index()
+    parallel_1_df = df[df['OPERATION_MODE'] == 4].groupby('SIZE').median().reset_index()
+    parallel_2_df = df[df['OPERATION_MODE'] == 5].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(parallel_1_df['SIZE'], line_df['TIME'] / parallel_1_df['TIME'], label='Parallel V.1', marker='.')
+        plt.plot(parallel_2_df['SIZE'], line_df['TIME'] / parallel_2_df['TIME'], label='Parallel V.2', marker='.')
+
+        plt.xticks(range(0, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Speedup of Parallel Line')
+        plt.xlabel('Size')
+        plt.ylabel('Speedup')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'parallel-speedup.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_parallel_efficiency(df: pd.DataFrame) -> None:
+    line_df = df[df['OPERATION_MODE'] == 2].groupby('SIZE').median().reset_index()
+    parallel_1_df = df[df['OPERATION_MODE'] == 4].groupby('SIZE').median().reset_index()
+    parallel_2_df = df[df['OPERATION_MODE'] == 5].groupby('SIZE').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(parallel_1_df['SIZE'], line_df['TIME'] / parallel_1_df['TIME'] / 8, label='Parallel V.1', marker='.')
+        plt.plot(parallel_2_df['SIZE'], line_df['TIME'] / parallel_2_df['TIME'] / 8, label='Parallel V.2', marker='.')
+
+        plt.xticks(range(0, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('Efficiency of Parallel Line')
+        plt.xlabel('Size')
+        plt.ylabel('Efficiency')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'parallel-efficiency.pdf')
         plt.savefig(output_path)
         plt.close()
 
@@ -71,5 +244,13 @@ if __name__ == '__main__':
     os.makedirs(output_folder, exist_ok=True)
 
     (cpp_df, lua_df) = import_csv_files(folder_path)
-    plot_lang_comp(cpp_df, lua_df)
+
+    plot_naive_line_time(cpp_df, lua_df)
+    plot_naive_line_flops(cpp_df, lua_df)
+    plot_line_block_time(cpp_df)
+    plot_line_block_flops(cpp_df)
+    plot_parallel_time(cpp_df)
+    plot_parallel_flops(cpp_df)
+    plot_parallel_speedup(cpp_df)
+    plot_parallel_efficiency(cpp_df)
 
