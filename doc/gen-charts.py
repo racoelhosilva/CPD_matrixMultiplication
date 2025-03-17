0f3b2a23cb2cc443ff6be46aa0d1e9115f6b2367
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-03-17 08:27:55 +0000

    Generate more graphs

--- doc/gen-charts.py
+++ doc/gen-charts.py
@@ -11,7 +11,7 @@ plot_theme = load_theme('ambivalent').set_font(sans_serif='Lato')
 def import_csv_files(folder_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     cpp_dfs = []
     for filename in os.listdir(folder_path):
-        if not filename.startswith('test-cpp') or not filename.endswith('.csv'):
+        if not filename.startswith('test-cpp') or not filename.endswith('.csv') or filename.endswith('l3.csv'):
             continue
 
         filepath = os.path.join(folder_path, filename)
@@ -19,6 +19,17 @@ def import_csv_files(folder_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
         cpp_dfs.append(df)
 
     cpp_df = pd.concat(cpp_dfs, ignore_index=True)
+
+    cpp_l3_dfs = []
+    for filename in os.listdir(folder_path):
+        if not filename.startswith('test-cpp') or not filename.endswith('l3.csv'):
+            continue
+
+        filepath = os.path.join(folder_path, filename)
+        df = pd.read_csv(filepath)
+        cpp_l3_dfs.append(df)
+
+    cpp_l3_df = pd.concat(cpp_l3_dfs, ignore_index=True)
 
     lua_dfs = []
     for filename in os.listdir(folder_path):
@@ -31,7 +42,7 @@ def import_csv_files(folder_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
     lua_df = pd.concat(lua_dfs, ignore_index=True)
 
-    return (cpp_df, lua_df)
+    return (cpp_df, cpp_l3_df, lua_df)
 
 def plot_naive_line_time(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
     naive_cpp_df = cpp_df[cpp_df['OPERATION_MODE'] == 1].groupby('M').median().reset_index()
@@ -85,6 +96,30 @@ def plot_naive_line_flops(cpp_df: pd.DataFrame, lua_df: pd.DataFrame) -> None:
         plt.savefig(output_path)
         plt.close()
 
+def plot_naive_line_cache(df: pd.DataFrame) -> None:
+    naive_df = df[df['OPERATION_MODE'] == 1].groupby('M').median().reset_index()
+    line_df = df[(df['OPERATION_MODE'] == 2) & (df['M'] <= 3000)].groupby('M').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(naive_df['M'], naive_df['L1 DCM'] / 1e9, label='Naive L1 DCM', marker='.')
+        plt.plot(naive_df['M'], naive_df['L2 DCM'] / 1e9, label='Naive L2 DCM', marker='.')
+        plt.plot(line_df['M'], line_df['L1 DCM'] / 1e9, label='Line L1 DCM', marker='.')
+        plt.plot(line_df['M'], line_df['L2 DCM'] / 1e9, label='Line L2 DCM', marker='.')
+
+        plt.xticks(range(600, 3001, 400))
+        plt.ylim(bottom=0)
+
+        plt.title('Cache Misses of Naive vs Line')
+        plt.xlabel('Size')
+        plt.ylabel('Billion Cache Misses')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'naive-line-cache.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
 def plot_line_block_time(df: pd.DataFrame) -> None:
     line_df = df[(df['OPERATION_MODE'] == 2) & (df['M'] >= 4096)].groupby('M').median().reset_index()
     block_128_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 128)].groupby('M').median().reset_index()
@@ -134,6 +169,59 @@ def plot_line_block_flops(df: pd.DataFrame) -> None:
         plt.legend()
 
         output_path = os.path.join(output_folder, 'line-block-flops.pdf')
+        plt.savefig(output_path)
+        plt.close()
+
+def plot_line_block_cache(df: pd.DataFrame) -> None:
+    line_df = df[(df['OPERATION_MODE'] == 2) & (df['M'] >= 4096)].groupby('M').median().reset_index()
+    block_128_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 128)].groupby('M').median().reset_index()
+    block_256_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 256)].groupby('M').median().reset_index()
+    block_512_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 512)].groupby('M').median().reset_index()
+
+    for cache_miss in ['L1 DCM', 'L2 DCM']:
+        with plot_theme:
+            plt.figure()
+
+            plt.plot(line_df['M'], line_df[cache_miss] / 1e9, label='Line', marker='.')
+            plt.plot(block_128_df['M'], block_128_df[cache_miss] / 1e9, label='Block-128', marker='.')
+            plt.plot(block_256_df['M'], block_256_df[cache_miss] / 1e9, label='Block-256', marker='.')
+            plt.plot(block_512_df['M'], block_512_df[cache_miss] / 1e9, label='Block-512', marker='.')
+
+            plt.xticks(range(4096, 10241, 2048))
+            plt.ylim(bottom=0)
+
+            plt.title(cache_miss[:2] + ' Cache Misses of Line and Block per Block Size')
+            plt.xlabel('Size')
+            plt.ylabel('Billion Cache Misses')
+            plt.legend()
+
+            output_path = os.path.join(output_folder, 'line-block-' + cache_miss[:2].lower() + '.pdf')
+            plt.savefig(output_path)
+            plt.close()
+
+def plot_line_block_l3(df: pd.DataFrame) -> None:
+    line_df = df[(df['OPERATION_MODE'] == 2) & (df['M'] >= 4096)].groupby('M').median().reset_index()
+    block_128_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 128)].groupby('M').median().reset_index()
+    block_256_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 256)].groupby('M').median().reset_index()
+    block_512_df = df[(df['OPERATION_MODE'] == 3) & (df['BLOCK_SIZE'] == 512)].groupby('M').median().reset_index()
+
+    with plot_theme:
+        plt.figure()
+
+        plt.plot(line_df['M'], line_df['L3 TCM'] / 1e9, label='Line', marker='.')
+        plt.plot(block_128_df['M'], block_128_df['L3 TCM'] / 1e9, label='Block-128', marker='.')
+        plt.plot(block_256_df['M'], block_256_df['L3 TCM'] / 1e9, label='Block-256', marker='.')
+        plt.plot(block_512_df['M'], block_512_df['L3 TCM'] / 1e9, label='Block-512', marker='.')
+
+        plt.xticks(range(4096, 10241, 2048))
+        plt.ylim(bottom=0)
+
+        plt.title('L3 Cache Misses of Line and Block per Block Size')
+        plt.xlabel('Size')
+        plt.ylabel('Billion Cache Misses')
+        plt.legend()
+
+        output_path = os.path.join(output_folder, 'line-block-l3.pdf')
         plt.savefig(output_path)
         plt.close()
 
@@ -243,12 +331,15 @@ if __name__ == '__main__':
     output_folder = 'latex/pdf/'
     os.makedirs(output_folder, exist_ok=True)
 
-    (cpp_df, lua_df) = import_csv_files(folder_path)
+    (cpp_df, cpp_l3_df, lua_df) = import_csv_files(folder_path)
 
     plot_naive_line_time(cpp_df, lua_df)
     plot_naive_line_flops(cpp_df, lua_df)
+    plot_naive_line_cache(cpp_df)
     plot_line_block_time(cpp_df)
     plot_line_block_flops(cpp_df)
+    plot_line_block_cache(cpp_df)
+    plot_line_block_l3(cpp_l3_df)
     plot_parallel_time(cpp_df)
     plot_parallel_flops(cpp_df)
     plot_parallel_speedup(cpp_df)
